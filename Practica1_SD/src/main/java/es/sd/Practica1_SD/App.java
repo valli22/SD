7a85c35a9b374d98898ba68227458f000f57bc02
valli22 <davidvacasmiguel@hotmail.com>
2016-03-09 14:24:14 +0100

    proyecto creado

---
+++ Practica1_SD/src/main/java/es/sd/Practica1_SD/App.java
@@ -0,0 +1,13 @@
+package es.sd.Practica1_SD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
