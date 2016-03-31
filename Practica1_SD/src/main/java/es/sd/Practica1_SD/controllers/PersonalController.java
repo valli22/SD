e86c1b6ddc43c449de0cd4ce387da22b65ae5511
valli22 <davidvacasmiguel@hotmail.com>
2016-03-31 20:54:59 +0200

    problemas con recogida de parametros

--- Practica1_SD/src/main/java/es/sd/Practica1_SD/controllers/PersonalController.java
+++ Practica1_SD/src/main/java/es/sd/Practica1_SD/controllers/PersonalController.java
@@ -1,9 +1,14 @@
 package es.sd.Practica1_SD.controllers;
 
+import java.io.Console;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.sd.Practica1_SD.modelos.Empleado;
@@ -35,5 +40,29 @@ public class PersonalController {
 			rep.save(emp);
 			
 			return "addCorrecto";
+		}
+		
+		@RequestMapping(value="/consultasPersonal")
+		public String buscar(Model model){
+			
+			return "BuscarEmpleado";
+		}
+		
+		@RequestMapping(value="/consultasEmpleadoTipo")
+		public String consultarTodos(@ModelAttribute String busqueda,Model model){
+			System.out.println(busqueda);
+			List<Empleado> busquedaEmp = rep.findAllByTipo(busqueda);
+			model.addAttribute("listaBusquedaTipo",busquedaEmp);
+			
+			return "BuscarEmpleado";
+		}
+		
+		@RequestMapping(value="/consultasEmpleadoApellidos")
+		public String consultarApellidos(Model model){
+			
+			List<Empleado> busquedaEmp = rep.findAll();
+			model.addAttribute("listaBusquedaTodos",busquedaEmp);
+			
+			return "BuscarEmpleado";
 		}
 }
