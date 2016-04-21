d19a4fd8cdf62659f7f0712078f8aafe29892787
valli22 <davidvacasmiguel@hotmail.com>
2016-04-21 10:55:26 +0200

    html formulario

--- Practica2_SD/practica2.js
+++ Practica2_SD/practica2.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
 	
-	$('.img').click(function() {
-		
-		type = $(this).attr('data-type');
-		
-		$('.overlay-container').fadeIn(function() {
-			
-			window.setTimeout(function(){
-				$('.window-container.'+type).addClass('window-container-visible');
-                console.log("entra");
-			}, 100);
-			
-		});
-	});
-	
-	$('.close').click(function() {
-		$('.overlay-container').fadeOut().end().find('.window-container').removeClass('window-container-visible');
-	});
 	
 });
--- Practica2_SD/practica2SD.js
+++ Practica2_SD/practica2SD.js
@@ -1,23 +1,25 @@
 $(document).ready(function () {
-    var date = encodeURI('0000-01-01 00:00:00')
+    var date = encodeURI('')
     var contentType = 7
     console.log(date)
     var argMinTakenDate = '&min_taken_date=' + date; //añadir esto a la url para buscar por min_taken_date
-    var argDescription = 'Esto es una prueba'
+    var argDescription = ''
     var argContentType = '&content_type=' + contentType
-    var argTag = 'animal'
+    var argTag = ''
     var argMinWidth = 0
     var argMinHeight = 0
     var argMaxWidht = 5000
     var argMaxHeight = 5000
-    var argViews = 2
+    var argViews = 0
     var url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=' + api_key + '&extras=url_s,tags,views,description&user_id=' + user_id + '&format=json&nojsoncallback=1' + argContentType + argMinTakenDate
     console.log(url)
     $.ajax({
         url: url
     }).done(function (data) {
+        var i = 0;
         data.photos.photo.forEach(function (foto) {
             console.log(foto)
+            
             var width = foto.width_s
             var height = foto.height_s
             var withTag = foto.tags.indexOf(argTag) > -1
@@ -42,10 +44,29 @@ $(document).ready(function () {
                 //Para sacar las fotos que tengan X frase o palabra su descripcion
                 /*if (foto.description._content.indexOf(argDescription) > -1) {
                     $('body').append('<img src="https://farm' + foto.farm + '.staticflickr.com/' + foto.server + '/' + foto.id + '_' + foto.secret + '.jpg"/>')*/
-                $('body').append('<img src="https://farm' + foto.farm + '.staticflickr.com/' + foto.server + '/' + foto.id + '_' + foto.secret + '.jpg"/>')
+                $('#lista').append('<img value="'+i+'" src="https://farm' + foto.farm + '.staticflickr.com/' + foto.server + '/' + foto.id + '_' + foto.secret + '.jpg" data-type="zoomin" class="img"/><div class="overlay-container"><div class="window-container zoomin"><img id=fotito src="https://farm' + foto.farm + '.staticflickr.com/' + foto.server + '/' + foto.id + '_' + foto.secret + '.jpg" class="img"><span class="close">Cerrar</span></div></div>')
             }
+            i++;
         })
-    })
+        $('.img').click(function() {
+		console.log("entra1");
+		type = $(this).attr('data-type');
+        console.log(this);
+		console.log($(this).val());
+		$('.overlay-container').fadeIn(function() {
+			
+			window.setTimeout(function(){
+				$('.window-container.'+type).addClass('window-container-visible');
+                console.log("entra");
+			}, 100);
+			
+		});
+        });
 
+        $('.close').click(function() {
+            $('.overlay-container').fadeOut().end().find('.window-container').removeClass('window-container-visible');
+        });
+    })
+	
 
 })
